ce700ce31626b156a70ff6b77279205331f6a44b
Youssef Sakini <youssef0sakini@gmail.com>
2021-05-17 17:27:19 +0100

    form validator (SignUp Validation)

--- src/app/ui/shared/globalSignLog/sinscrire/sinscrire/sinscrire.component.ts
+++ src/app/ui/shared/globalSignLog/sinscrire/sinscrire/sinscrire.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { NgForm, FormGroup, FormControl, Validators } from '@angular/forms';
+
 
 @Component({
   selector: 'app-sinscrire',
@@ -7,9 +9,64 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SinscrireComponent implements OnInit {
 
-  constructor() { }
+// @ViewChild('f') signUpForm!: NgForm ;
+signUpForm!:FormGroup;
+
+  // nom!:string;
+  // prenom!:string;
+  // email!:string;
+  // password!:string;
+  // type!: ['Giver','Beneficiary'];
+  // adresse!:string;
+  // tel!:string;
+
+// submitted:boolean =false;
+  constructor() {}
+
+  // =====Submit the form======
+  // onFormSubmit(){
+  //   this.submitted=true;
+  //   this.nom=this.signUpForm.value.nom;
+  //   this.prenom=this.signUpForm.value.prenom;
+  //   this.email=this.signUpForm.value.email;
+  //   this.password=this.signUpForm.value.password;
+  //   this.type=this.signUpForm.value.type;
+  //   this.adresse=this.signUpForm.value.adresse;
+  //   this.tel=this.signUpForm.value.tel;
+  //   // Refresh the form
+  //   this.signUpForm.reset();
+  // }
+
+// show the data in form
+  // fillValues()
+  // {
+  //   this.signUpForm.form.patchValue({
+     
+  //       nom:"alan",
+  //       prenom:"walker",
+  //       email:"alanwalker@gmail.com",
+  //       password:"123456",
+  //       type:"beneficiary",
+  //       adresse:"los angles",
+  //       tel:"+341584452168"
+  //   }
+  //   )
+  // }
+
+  onSubmit(){
+    console.log(this.signUpForm);
+  }
 
   ngOnInit(): void {
+    this.signUpForm=new FormGroup({
+      'nom':new FormControl(Validators.required),
+      'prenom':new FormControl(Validators.required),
+      'email':new FormControl([Validators.required,Validators.email]),
+      'password':new FormControl(),
+      'type':new FormControl(),
+      'adresse':new FormControl(),
+      'tel':new FormControl()
+    })
   }
 
 }
